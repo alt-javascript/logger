69b4c361528c1996ae192b39fee3a46bda26897b
parravicinic <Craig.Parravicini2@tbcorp.com.au>
2022-03-08 10:35:08 +1000

    Experiments with rollup

---
+++ index-browser.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/extensions */
+export { default as ConfigurableLogger } from './ConfigurableLogger.js';
+export { default as ConsoleLogger } from './ConsoleLogger.js';
+export { default as DelegatingLogger } from './DelegatingLogger.js';
+export { default as JSONFormatter } from './JSONFormatter.js';
+export { default as Logger } from './Logger.js';
+export { default as LoggerFactory } from './LoggerFactory.js';
+export { default as LoggerLevel } from './LoggerLevel.js';
+export { default as LoggerCategoryCache } from './LoggerCategoryCache.js';
+export { default as PlainTextFormatter } from './PlainTextFormatter.js';
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 export default [
-  // Monolithic ESM bundle for browsers and deno.
+  // Monolithic ESM bundle for browsers and demo.
   {
-    input: 'modules/index.js',
+    input: 'index.js',
     treeshake: true,
     output: {
-      file: 'alt-javascript-logger-esm.js',
+      file: 'dist/alt-javascript-logger-esm.js',
       format: 'esm',
       strict: false,
       externalLiveBindings: false,
@@ -14,11 +14,38 @@ export default [
     },
   },
   {
-    input: 'modules/index.js',
+    input: 'index-browser.js',
     treeshake: true,
     output: {
-      file: 'alt-javascript-logger-umd.js',
-      name: 'alt-javascript-logger-umd',
+      file: 'dist/test-esm.js',
+      format: 'esm',
+      strict: false,
+      externalLiveBindings: false,
+      freeze: false,
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+    },
+  },
+  {
+    input: 'index-browser.js',
+    treeshake: true,
+    output: {
+      file: 'dist/test-iife.js',
+      format: 'iife',
+      name: 'alt',
+      strict: false,
+      externalLiveBindings: false,
+      freeze: false,
+      sourcemap: true,
+      sourcemapExcludeSources: true,
+    },
+  },
+  {
+    input: 'index.js',
+    treeshake: true,
+    output: {
+      file: 'dist/alt-javascript-logger-umd.js',
+      name: 'alt',
       format: 'umd',
       strict: false,
       externalLiveBindings: false,
